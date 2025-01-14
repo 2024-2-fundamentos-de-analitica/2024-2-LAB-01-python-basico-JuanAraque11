9615f4782483720261b3406cd3cb45d2e75547d0
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2025-01-14 09:52:09 -0500

    Update pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-
+import csv
 
 def pregunta_01():
     """
@@ -14,3 +14,18 @@ def pregunta_01():
     214
 
     """
+    
+    with open("files/input/data.csv", 'r') as file:
+        # Leer el archivo CSV separado por espacios
+        reader = csv.reader(file, delimiter=' ')
+
+        # Imprimir las primeras dos filas
+        for i in range(2):
+            print(next(reader))
+
+
+    #    second_column_sum = sum(int(row[1]) for row in reader)
+    #print(second_column_sum)
+    # return second_column_sum
+
+pregunta_01()
